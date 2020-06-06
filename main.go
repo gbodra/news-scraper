50508645fd6d7594941de320924128998444211d
Gustavo Bodra <gustavobodra@yahoo.com.br>
2020-06-06 12:27:54 -0300

    Ajuste na formatacao das mensagens

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/gocolly/colly"
@@ -16,6 +17,8 @@ func main() {
 	}
 
 	domain := "enterprisersproject.com"
+	message := ""
+
 	c := colly.NewCollector(
 		colly.AllowedDomains(domain),
 	)
@@ -26,8 +29,11 @@ func main() {
 
 		log.Printf("Link found: %q -> %s\n", title, domain+titleURL)
 
-		message := domain + titleURL
-		queryParams := "chat_id=" + os.Getenv("CHAT_ID") + "&text=" + message
+		message = message + "- <a href=\"https://" + domain + titleURL + "\">" + title + "</a>\n\n"
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		queryParams := "chat_id=" + os.Getenv("CHAT_ID") + "&text=" + url.QueryEscape(message) + "&parse_mode=HTML&disable_web_page_preview=true"
 
 		resp, err := http.Get("https://api.telegram.org/bot" + os.Getenv("TG_TOKEN") + "/sendMessage?" + queryParams)
 		if err != nil {
